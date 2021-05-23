91f5b6b58f476dfb97b226a0ab126180ab7129e5
Leah King <dancingheart714@yahoo.com>
2021-05-23 15:32:25 -0400

    set up folders/files/server/heroku/postgress

--- api/server.js
+++ api/server.js
@@ -1,10 +1,18 @@
-const express = require('express')
-const helmet = require('helmet')
-const cors = require('cors')
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const restrict = require('./middleware/restrict');
 
-const server = express()
-server.use(express.json())
-server.use(helmet())
-server.use(cors())
+const authRouter = require('./auth/auth-router');
+const usersRouter = require('./users/user-router');
+const plantsRouter = require('./plants/plants-router');
+const server = express();
+server.use(express.json());
+server.use(helmet());
+server.use(cors());
 
-module.exports = server
+server.use('/api/users', restrict, usersRouter);
+server.use('/api/plants', restrict, plantsRouter);
+server.use(authRouter);
+
+module.exports = server;
