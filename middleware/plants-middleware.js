7e1f3adc0015040d04ac5b1901f7baa8c333905b
Leah King <dancingheart714@yahoo.com>
2021-05-23 15:45:52 -0400

    restrict middleware built

--- middleware/plants-middleware.js
+++ middleware/plants-middleware.js
@@ -1,18 +1,1 @@
-const Plant = require('../plants/plants-model');
 
-const plantPayload = (req, res, next) => {
-  const { nickname, species, h2ofrequency } = req.body;
-  if (!nickname || !species || !h2ofrequency) {
-    res
-      .status(400)
-      .json({
-        message: 'nickname, species, and h2ofrequency fields are required',
-      });
-  } else {
-    next();
-  }
-};
-
-module.exports = {
-  plantPayload,
-};
